77e74593ce2864fc99327772a9367118e095aea7
Nelson Acosta <nelson.acosta.web@gmail.com>
2022-07-01 17:51:19 -0400

    feat: Add Initial ButtonCart Component

---
+++ src/components/ButtonCart.tsx
@@ -0,0 +1,9 @@
+const ButtonCart = () => {    
+    return (
+        <button className="btn-cart" onClick={console.log('click')}>
+            Add to cart
+        </button>
+    );
+}
+
+export default ButtonCart
