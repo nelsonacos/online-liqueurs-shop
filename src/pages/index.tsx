30ae2aa1d2fb7818ba3191eb5516c7f1aecf10b6
Nelson Acosta <nelson.acosta.web@gmail.com>
2022-07-02 18:18:24 -0400

    modify some details of for autocomplete help

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,26 @@
 import type { NextPage, GetStaticProps, InferGetStaticPropsType } from 'next'
 import { getData } from '../helpers/normalize'
 import Layout from '../components/Layout'
-
-type Product = {
-  product_id: string,
-  name: string,
-  total_price: string,
-}
+import Product from '../components/Product'
+import styleProduct from "../styles/Product.module.css"
+import ProductInfo from '../interfaces/Product'
 
 const Home: NextPage = ({ products }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Layout title="Online Liqueurs Shop">
-      <h1> Online Store </h1>
+      <h1> Online Liqueurs Shop </h1>
+      <div className={styleProduct.items}>
+        {products &&
+          products.map(( product: ProductInfo ) => (
+            <Product key={product.product_id} item={product} showAs="default" />
+          ))}
+      </div>
     </Layout>
   )
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const products: Product[] = await getData('http://localhost:6000/products');
+  const products: ProductInfo[] = await getData('http://localhost:6000/products');
 
   return {
     props: { products }
