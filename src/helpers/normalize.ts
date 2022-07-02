07e679d59d0939b0c0c61e7415fc3511fe7285e9
Nelson Acosta <nelson.acosta.web@gmail.com>
2022-07-02 18:09:32 -0400

    feat: add helpers for fetch data by id

--- src/helpers/normalize.ts
+++ src/helpers/normalize.ts
@@ -4,3 +4,25 @@ export const getData = async (url: string) => {
 
   return data;
 };
+
+export async function getPathById() {
+  const data = await getData("http://localhost:6000/products");
+  const ids = data.map((item) => {
+    return {
+      params: {
+        id: item.product_id,
+      }
+    }
+  });
+
+  return ids
+}
+
+export async function getProductById(id: string) {
+  const data = await getData("http://localhost:6000/products");
+  const product = data.find((item) => item.product_id === id);
+  return {
+    id,
+    data: product,
+  };
+}
