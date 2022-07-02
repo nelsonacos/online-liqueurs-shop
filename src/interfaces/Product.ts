39426b7a765c9703ac3c9b21b4999636b48591ce
Nelson Acosta <nelson.acosta.web@gmail.com>
2022-07-02 18:07:21 -0400

    refactor: Add interface Product

---
+++ src/interfaces/Product.ts
@@ -0,0 +1,18 @@
+export default interface Product {
+  allowed_packs: [];
+  categories: [];
+  compare_at_price: string;
+  description: string;
+  discount_percentage: number;
+  handle: string;
+  image_url: string;
+  list_price_id: string;
+  name: string;
+  price_per_litre: string;
+  price_per_unit: string;
+  product_id: string;
+  size: number;
+  sku: string;
+  total_price: string;
+  units_per_pack: number;
+}
