97a6c8d3d14c673cfa3c721f27ba2051e049f799
Nelson Acosta <nelson.acosta.web@gmail.com>
2022-07-04 17:56:29 -0400

    feat: Add Image Component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 import styles from "../styles/Product.module.css"
 import ButtonCart from "./ButtonCart"
 import ProductInfo from "../interfaces/Product"
@@ -6,13 +7,21 @@ import React from "react"
 
 type ProductProps = {
     item: ProductInfo,
-    showAs: string, 
+    showAs?: string,
 }
 
 const Product = ({ item, showAs }: ProductProps) => {
     if (showAs === "Page") {
         return (
             <div className={styles.page}>
+                <div className={styles.image}>
+                    <Image
+                        src={item.image_url}
+                        alt="item"
+                        width={500}
+                        height={500}
+                    />
+                </div>
                 <div className={styles.info}>
                     <div>
                         <h2>{item.name}</h2>
@@ -30,6 +39,14 @@ const Product = ({ item, showAs }: ProductProps) => {
         return (
             <div className={styles.listItem}>
                 <div>
+                    <Image
+                        src={item.image_url}
+                        alt="cart item"
+                        width={100}
+                        height={100}
+                    />
+                </div>
+                <div>
                     <div>{item.name}</div>
                     <div>${item.total_price}</div>
                 </div>
@@ -39,6 +56,18 @@ const Product = ({ item, showAs }: ProductProps) => {
 
     return (
         <div className={styles.item}>
+            <div>
+                <Link href={`/product/${item.product_id}`}>
+                    <a>
+                        <Image
+                            src={item.image_url}
+                            alt="Product Details Image"
+                            width={250}
+                            height={250}
+                        />
+                    </a>
+                </Link>
+            </div>
             <div>
                 <h3>
                     <Link href={`/product/${item.product_id}`}>
